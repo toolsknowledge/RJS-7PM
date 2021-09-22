92b541e275d8c7bbda638c264f0eef55496e49e6
Sambasivarao <samba.pendela@gmail.com>
2021-09-22 19:48:42 +0530

    code

--- forms-app/src/Signup.js
+++ forms-app/src/Signup.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Formik,Form } from "formik";
+import * as Yup from "yup";
+import TextField from "./TextField";
 
 const Signup = ()=>{
+
+    const validate = Yup.object({
+        firstName : Yup.string().
+                    max(15,"can't exceed 15 characters").
+                    required("Required")
+    });
+
+
     return(
         <React.Fragment>
-            Signup Implementation Soon...!
+            <Formik initialValues={{firstName:""}} 
+                    validationSchema={validate}
+                    onSubmit={values=>{
+                        console.log(values);
+                    }}>
+                <Form>
+                    <h1>Signup</h1>
+                    <TextField label="firstName"
+                               name="firstName"
+                               type="text"
+                               placeholder="enter first name"></TextField>
+                </Form>
+            </Formik>
         </React.Fragment>
     )
 };
---
+++ forms-app/src/TextField.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useField,ErrorMessage } from "formik";
+const TextField = ({label,...props})=>{
+    
+    const [field,meta] = useField(props);
+    
+    return(
+        <React.Fragment>
+            <div className="mb-2">
+                <label>{label}</label>
+                <input className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`}
+                    {...field} {...props} autoComplete="off">
+                </input>
+                <ErrorMessage 
+                    component="div" 
+                    name={field.name} 
+                    className="error" />
+            </div>
+        </React.Fragment>
+    )
+};
+export default TextField;
